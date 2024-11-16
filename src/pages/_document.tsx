0920c2c351a97e4fefbc9ea299786a9794ec9a81
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-16 17:54:58 +0100

    feat: add peano-gosper curve as background

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { LSystemRenderer, PEANO_GOSPER } from "@/components/lsystem";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -5,6 +6,15 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <div id="background">
+          <LSystemRenderer
+            iterations={6}
+            lsystem={PEANO_GOSPER}
+            length={7}
+            margin={5}
+            stroke={{ color: "black", width: 1 }}
+          />
+        </div>
         <div>
           <Main />
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import { doom, useAsciiText } from "react-ascii-text";
 import { PAGES } from "@/pages";
 import Title from "@/components/title";
-import { iterate, LSystemRenderer, PEANO_GOSPER } from "@/lsystem";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +9,6 @@ export default function Home() {
   return (
     <div>
       <Title text="Ludovic Mermod" />
-      <LSystemRenderer
-        iterations={6}
-        lsystem={PEANO_GOSPER}
-        length={5}
-        margin={5}
-        stroke={{ color: "black", width: 1 }}
-      />
       <p>Master student in Computer Science at EPFL</p>
       <p>Type :g &lt;page&gt; to go to a page:</p>
       <ul>
