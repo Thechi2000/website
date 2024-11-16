646aa562b94a5dddef517007ecebe6a31c09183f
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-16 20:16:46 +0100

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import Breadcrumbs from "@/components/breadcrumbs";
+import Title from "@/components/title";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <Title text="ENOENT" />
+      <Link className="text-center" href="/"><h3>---&gt; Back to home &lt;---</h3></Link>
+    </>
+  );
+}
