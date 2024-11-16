706e02671d719b91de22ce41bf7e5e852a88e515
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-16 22:58:19 +0100

    feat: add link to repository on project pages

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -3,6 +3,7 @@ import Title from "@/components/title";
 import { Data } from "@/models";
 import { NextJSMarkdown } from "@/utils";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { readFile } from "node:fs/promises";
 
@@ -16,6 +17,13 @@ export default function Page({
     <>
       <Title text={project.name} />
       <Breadcrumbs />
+      {project.repository ? (
+        <Link className="text-center" href={project.repository} target="_blank">
+          --&gt; See the repository &lt;--
+        </Link>
+      ) : (
+        <> </>
+      )}
       <NextJSMarkdown origin={project.resourcesBaseUrl}>
         {markdown}
       </NextJSMarkdown>
