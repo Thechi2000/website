8169d2395c28243383b3f307e2216905c1f4509d
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-29 20:27:47 +0100

    fix: update webpack config to use modern sass compiler

--- next.config.js
+++ next.config.js
@@ -1,8 +1,37 @@
+const regexEqual = (x, y) => {
+  return (
+      x instanceof RegExp &&
+      y instanceof RegExp &&
+      x.source === y.source &&
+      x.global === y.global &&
+      x.ignoreCase === y.ignoreCase &&
+      x.multiline === y.multiline
+  )
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    silenceDeprecations: ["legacy-js-api"],
+  webpack: (config) => {
+    const oneOf = config.module.rules.find(
+      (rule) => typeof rule.oneOf === "object"
+    );
+
+    if (oneOf) {
+      const sassRule = oneOf.oneOf.find((rule) =>
+        regexEqual(rule.test, /\.module\.(scss|sass)$/)
+      );
+      if (sassRule) {
+        const sassLoader = sassRule.use.find((el) =>
+          el.loader.includes("next/dist/compiled/sass-loader")
+        );
+        if (sassLoader) {
+          sassLoader.loader = "sass-loader";
+        }
+      }
+    }
+
+    return config;
   },
 };
 
