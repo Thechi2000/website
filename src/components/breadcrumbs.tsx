8e3f6a449d3ef59168de715132cf092c48ead955
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-06-01 13:35:29 +0200

    fix: use pathname to generate breadcrumbs

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,36 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import styles from "@/styles/Breadcrumbs.module.scss";
 import Link from "next/link";
 import { toDisplayString } from "@/string";
-import { useEffect, useRef, useState } from "react";
 
 export default function Breadcrumbs() {
-  const router = useRouter();
-  const [path, setPath] = useState<string[]>([]);
-  const retrieved = useRef(false);
+  const path = usePathname();
 
-  useEffect(() => {
-    if (retrieved.current) return;
-    retrieved.current = true;
-
-    setPath(window.location.pathname.split("/").filter((e) => e.length !== 0));
-  }, []);
-
-  return path.length === 0 ? (
-    <></>
-  ) : (
+  return (
     <p className={styles.breadcrumbs}>
       /<Link href={"/"}>Root</Link>
-      {path.map((segment, index, array) => (
-        <span key={index}>
-          /
-          <Link href={`/${array.slice(0, index + 1).join("/")}`}>
-            {toDisplayString(segment)}
-          </Link>
-        </span>
-      ))}
+      {path
+        .split("/")
+        .filter((e) => e.length !== 0)
+        .map((segment, index, array) => (
+          <span key={index}>
+            /
+            <Link href={`/${array.slice(0, index + 1).join("/")}`}>
+              {toDisplayString(segment)}
+            </Link>
+          </span>
+        ))}
     </p>
   );
 }
