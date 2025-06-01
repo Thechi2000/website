a04fb757fbf5fae9e3c7d81004f6b9f7cb5aadda
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-06-01 10:09:56 +0200

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { PAGES } from "@/pages";
+import { fetchDataServerSideProps } from "@/utils";
+import type { MetadataRoute } from "next";
+
+const BASE_URL = "https://lmermod.ch";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const data = (await fetchDataServerSideProps()).props;
+
+  // @ts-expect-error
+  return [
+    ...Object.entries(PAGES).map((page) => ({
+      url: `${BASE_URL}/${page[1]}`,
+      changeFrequency: "monthly",
+      lastModified: new Date(),
+      priority: 1,
+    })),
+    ...data.projects.map((project) => ({
+      url: `${BASE_URL}/projects/${project.id}`,
+      changeFrequence: "monthly",
+      lastModified: new Date(),
+      priority: 0.7,
+    })),
+  ];
+}
