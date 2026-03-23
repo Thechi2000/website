e525002fd9b17b69039acf2282cfe888fe5cff18
Lucie Mermod <lucie.mermod@gmail.com>
2026-03-23 17:48:30 +0100

    fix: disable caching for fetchData

--- src/fetch.ts
+++ src/fetch.ts
@@ -6,12 +6,13 @@ export async function fetchData(): Promise<Data> {
     await fetch(
       `${WEBSITE_BASE_URL}/data/data${
         process.env.NODE_ENV !== "production" ? ".dev" : ""
-      }.json`
+      }.json`,
+      { cache: "no-cache" },
     )
   ).json();
 
   data.me.description = await fetch(data.me.description).then((res) =>
-    res.text()
+    res.text(),
   );
 
   return data;
